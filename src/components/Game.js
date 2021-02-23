d5309ab4b887f4209a5db9b4fa18e824f75532d1
jayyp35 <jprabhu35@gmail.com>
2021-02-24 00:44:37 +0530

    Introducing state. Function must be changed to a class based component so that we can track state using constructor

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,14 +1,24 @@
 import React from 'react'
-import Board from '/Board'
+import Board from './Board'
 
-export  default function Game() {
-    return (
+export  default class Game extends React.Component {
+    constructor() {
+        super() // calling super is a must
+        //We will define a state
+
+        this.state = {
+            xIsNext: true,
+            stepNumber: 0,
+            history : [
+                {squares: Array(9).fill(null)}
+            ]
+        }
+    }
+    render() {
         <div className="game">
             <div className="game-board">
                 <Board/>    
             </div>
         </div>
-
-        
-    )
+    }
 }
