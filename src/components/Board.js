d45fe6aa836a3e333ac0228c8459150b01fb7f0e
jayyp35 <jprabhu35@gmail.com>
2021-02-24 00:17:48 +0530

    Rendered Board component using Square Components'

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,2 +1,26 @@
 import React from 'react'
-import Square
+import Square from './Square'
+
+export default function Board() {
+    return ( 
+        <div>
+            <div className="border-app">
+                <Square/>
+                <Square/>
+                <Square/>
+            </div>
+
+            <div className="border-app">
+                <Square/>
+                <Square/>
+                <Square/>
+            </div>
+
+            <div className="border-app">
+                <Square/>
+                <Square/>
+                <Square/>
+            </div>
+        </div>
+    )
+}
