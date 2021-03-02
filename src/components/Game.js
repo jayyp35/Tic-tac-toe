ae49cd0cb49aeec38a64e768a14f3598e11de7d9
jayyp35 <jprabhu35@gmail.com>
2021-03-02 23:13:21 +0530

    Tic-tac-toe game

--- src/components/Game.js
+++ src/components/Game.js
@@ -11,14 +11,86 @@ export  default class Game extends React.Component {
             stepNumber: 0,
             history : [
                 {squares: Array(9).fill(null)}
-            ]
+            ],
+            
         }
     }
+
+    handleClick = (i) => {
+        const history = this.state.history
+        const current = history[history.length - 1]
+        const squares = current.squares
+        
+        if(squares[i]) {
+            return
+        } 
+        let winner = calculateWinner(squares)
+        if(winner) {
+            return
+        }
+
+        squares[i] = this.state.xIsNext ? 'X' : 'O'
+
+        this.setState( {
+            history: history.concat ({
+                squares: squares
+            }),
+            xIsNext: !this.state.xIsNext,
+            stepNumber: history.length
+        } )
+
+        winner = calculateWinner(squares)
+        if(winner) {
+            return
+        }
+    }
+    
+
     render() {
-        <div className="game">
-            <div className="game-board">
-                <Board/>    
+        const history = this.state.history
+        const current = history[history.length - 1] 
+        const squares = current.squares
+        let win = calculateWinner(squares)
+        let show
+        if(win) {
+           show = "Winner is " + win
+        } else {
+            show =( this.state.xIsNext ? 'X' : 'O' ) + "'s turn"
+        }
+        return(
+            <div className="game">
+                <div className="game-board">
+                    <Board clickAction={(i) => this.handleClick(i)} squares={squares}/>    
+                </div>
+                
+                <div className="winner">{show}</div>
             </div>
-        </div>
+        )
+    }
+
+}
+
+function calculateWinner(squares) {
+    
+    const possibilities = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [6,7,8]
+    ]
+
+    for(let i=0;i<possibilities.length;i++) {
+        const [a , b, c] = possibilities[i]
+        
+        
+        if(squares[a]) {
+            if(squares[a]==squares[b] && squares[b]==squares[c])
+            return squares[a]
+        }
+          
     }
 }
